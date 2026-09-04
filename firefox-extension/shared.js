5a71e946fd8b3a525e9234f9c9b3d59a3ed5b823
zrajm <zrajm@klingonska.org>
2026-09-04 13:32:15 +0200

    Renamed icon files

--- firefox-extension/shared.js
+++ firefox-extension/shared.js
@@ -4,23 +4,23 @@
 export const getCurrentTab = () =>
   browser.tabs.query({ active: true, currentWindow: true }).then(([x]) => x)
 
-export const errorIcon   = ['pic/like-error.svg',  'Unsupported page']
-export const defaultIcon = ['pic/like-normal.svg', 'Like page']
+export const errorIcon   = ['pic/thumbsup-dimmed.svg',  'Unsupported page']
+export const defaultIcon = ['pic/thumbsup.svg', 'Like page']
 export const categoryIcons = {
     '👍': {
         name: 'up',
-        normal: ['pic/like-normal.svg', 'Like page'],
-        hilite: ['pic/like-hilite.svg', 'Remove like'],
+        normal: ['pic/thumbsup.svg', 'Like page'],
+        hilite: ['pic/thumbsup-active.svg', 'Remove like'],
     },
     '👎': {
         name: 'down',
-        normal: ['pic/dislike-normal.svg', 'Dislike page'],
-        hilite: ['pic/dislike-hilite.svg', 'Remove dislike'],
+        normal: ['pic/thumbsdn.svg', 'Dislike page'],
+        hilite: ['pic/thumbsdn-active.svg', 'Remove dislike'],
     },
     '⭐': {
         name: 'star',
-        normal: ['pic/star-normal.svg', 'Star page'],
-        hilite: ['pic/star-hilite.svg', 'Remove star'],
+        normal: ['pic/bookmark.svg', 'Star page'],
+        hilite: ['pic/bookmark-active.svg', 'Remove star'],
     },
 }
 
